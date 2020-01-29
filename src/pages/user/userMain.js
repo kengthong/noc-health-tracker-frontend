f6a582ff6725e3cd794d6879c69dae8a7ee02c52
C.Junxiang <e0176013@u.nus.edu>
2020-01-30 04:15:54 +0800

    Temperature for update Details form done

--- src/pages/user/userMain.js
+++ src/pages/user/userMain.js
@@ -2,106 +2,39 @@ import React from 'react';
 import styled from 'styled-components';
 import { Container } from '@material-ui/core';
 import SideBar from '../../components/sidebar';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
-
-import FeelGoodPicture from '../../assets/feelgood.jpeg';
-import FeelBadPicture from '../../assets/feelbad.jpg';
+import UserStatusForm from './components/userStatus';
+import UserUpdateForm from './components/userUpdateForm';
 
 const UserMainContainer = styled(Container)`
   font-family: roboto;
 `;
 
-const ActionableGroup = styled.div`
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  margin-right: -50%;
-  transform: translate(-50%, -50%);
-  padding:20px;
-`;
-
-const StyledTitle = styled.text`
-  font-weight: 900;
-  font-size: xx-large;
-  padding-bottom: 20px;
-`;
-
-const FeelBadCard = styled.div`
-  width: 47%; 
-  float:right;
-  padding-top: 20px;
-`;
-
-const FeelGoodCard = styled.div`
-  width: 47%; 
-  float:left;
-  padding-top: 20px;
-`;
-
 export default class userMain extends React.Component {
   constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+        
+      };
   }
+
   componentDidMount = () => {};
+
+  updateStatus = status => {
+    console.log(status);
+    this.setState({
+      status: status,
+    })
+  }
+
   render() {
+    const { status } = this.state;
     return (
       <UserMainContainer max-width='xs'>
         <SideBar/>
-        <ActionableGroup>
-          <StyledTitle>Today I feel...</StyledTitle>
-          <div>
-
-          <Tooltip title="If you feel any signs of ... bla bla bla, you are not feeling well!" arrow>
-            <FeelGoodCard>
-              <Card>
-                <CardActionArea >
-                  <CardMedia
-                    image={FeelGoodPicture}
-                    component="img"
-                    height="180"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Good! 
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      I do not feel any symptoms such as Fever, Flu, Headache or diarrhoea!
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </FeelGoodCard>
-            </Tooltip>
-
-            <Tooltip title="If you feel any signs of ... bla bla bla, you are not feeling well!" arrow>
-            <FeelBadCard >
-              <Card>
-                <CardActionArea>
-                  <CardMedia
-                    image= {FeelBadPicture}
-                    component="img"
-                    height="180"
-                  />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Bad! 
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      I don't feel so good, mr stark.
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </FeelBadCard>
-            </Tooltip>
-          </div>
-        </ActionableGroup>
+        {!status && <UserStatusForm updateStatus={this.updateStatus}/>}
+        {status && 
+          <UserUpdateForm status={status}/>
+        } 
        </UserMainContainer>
     );
   }
