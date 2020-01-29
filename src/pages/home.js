d5d7409804ee195f45e345dbde7819c6af4959d8
C.Junxiang <e0176013@u.nus.edu>
2020-01-30 01:53:15 +0800

    Added Styling for Home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import SideBar from '../components/sidebar.js';
+import { Container } from '@material-ui/core';
+
+const HomeContainer = styled(Container)`
+  font-family: roboto;
+`;
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
+            <HomeContainer max-width='xs'>
                 HOME PAGE
-            </div>
+            </HomeContainer>
         )
     }
 }
